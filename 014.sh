634f836f11c144d92f6ef7b34502d6eae08161d1
Jeff Lavallee <jeff@zeroclue.com>
2009-10-06 07:49:41 -0700

    new syntax highlighting for some slides (requires new Vroom::Vroom)

---
+++ 014.sh
@@ -0,0 +1,23 @@
+
+
+
+
+                                CPAN::Mini crontab
+
+# m h dom mon dow command
+20  1   *   *   *   /usr/local/bin/minicpan 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 014z.sh
@@ -0,0 +1,21 @@
+
+
+
+
+                                CPAN::Mini crontab
+
+# m h dom mon dow command
+20  1   *   *   *   /usr/local/bin/minicpan 
+
+
+ * or mirror development versions:
+
+# m h dom mon dow command
+20  1   *   *   *   /usr/local/bin/minicpan -c CPAN::Mini::Devel 
+
+
+
+
+
+
+
